d4a7756dda75032a1446da8cf9b753e0c7948378
erharms <psharms@usf.edu>
2026-02-15 13:35:07 -0500

    Add files via upload

---
+++ harms.module5.4370.R
@@ -0,0 +1,15 @@
+#Find the value of inverse of a matrix, determinant of a matrix by using the following values:
+  A=matrix(1:100, nrow=10)
+  B=matrix(1:1000, nrow=10)
+#  Post your result and procedure you took on your blog.
+  #A good start will be:
+#>A <- matrix(1:100, nrow=10)  
+#>B <- matrix(1:1000, nrow=10)
+
+  
+  dim(A)
+  dim(B)
+  det(A)
+  solve(A)
+  det(B)
+  solve(B)
