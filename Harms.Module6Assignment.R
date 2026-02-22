e4d00610e4d703d5bb19d7e364ee570db4e3d515
erharms <psharms@usf.edu>
2026-02-22 17:30:53 -0500

    Add files via upload

---
+++ Harms.Module6Assignment.R
@@ -0,0 +1,11 @@
+A = matrix(c(2, 0, 1, 3), ncol=2)
+B = matrix(c(5, 2, 4, -1), ncol=2)
+result <- A + B
+print(A + B)
+
+diag(x = 4, 4, 1, 2, 3)
+
+X <- diag(3, 5, 5)
+X[1, ] <- c(3,1,1,1,1)
+X[2:5, 1] <- 2
+X
